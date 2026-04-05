983249436dead224cf1c127f015e40251143e314
mitumial <ace.vortex76@gmail.com>
2026-04-05 13:23:13 -0500

    add repository

---
+++ MS_Finanzas/src/main/java/com/example/demo/infra/persistence/repository/JpaTransactionRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.infra.persistence.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.infra.persistence.entity.TransactionEntity;
+
+@Repository
+public interface JpaTransactionRepository extends JpaRepository<TransactionEntity, Long>{
+
+}
---
+++ MS_Finanzas/src/main/java/com/example/demo/infra/persistence/repository/JpaTransactionRepositoryAdapter.java
@@ -0,0 +1,34 @@
+package com.example.demo.infra.persistence.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.example.demo.application.repository.TransactionRepositoryPort;
+import com.example.demo.domain.model.Transaction;
+import com.example.demo.infra.mapper.TransactionEntityMapper;
+import com.example.demo.infra.persistence.entity.TransactionEntity;
+
+@Component
+public class JpaTransactionRepositoryAdapter implements TransactionRepositoryPort{
+    private final JpaTransactionRepository jpaTransactionRepository;
+    private final TransactionEntityMapper transactionEntityMapper;
+
+    public JpaTransactionRepositoryAdapter(JpaTransactionRepository jpaTransactionRepository, TransactionEntityMapper transactionEntityMapper) {
+        this.jpaTransactionRepository = jpaTransactionRepository;
+        this.transactionEntityMapper = transactionEntityMapper;
+    }
+
+    @Override
+    public List<Transaction> findAll() {
+        return jpaTransactionRepository.findAll().stream().map(transactionEntityMapper::toDomain).toList();
+    }
+
+    @Override
+    public Transaction save(Transaction transaction) {
+        TransactionEntity transactionEntity = transactionEntityMapper.toEntity(transaction);
+        TransactionEntity savedTransactionEntity = jpaTransactionRepository.save(transactionEntity);
+        return transactionEntityMapper.toDomain(savedTransactionEntity);
+    }
+
+}
