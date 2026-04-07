1cbabb172191410cdbbe8d3fa8d35861b682783f
mitumial <ace.vortex76@gmail.com>
2026-04-07 18:29:35 -0500

    fix datatype for id in jparepository

--- MS_Finanzas/src/main/java/com/example/demo/infra/persistence/repository/JpaTransactionRepository.java
+++ MS_Finanzas/src/main/java/com/example/demo/infra/persistence/repository/JpaTransactionRepository.java
@@ -1,11 +1,13 @@
 package com.example.demo.infra.persistence.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.infra.persistence.entity.TransactionEntity;
 
 @Repository
-public interface JpaTransactionRepository extends JpaRepository<TransactionEntity, Long>{
+public interface JpaTransactionRepository extends JpaRepository<TransactionEntity, UUID>{
 
 }
